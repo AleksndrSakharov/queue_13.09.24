5c479a552e431e666d2eb3dfbc69046188bb102f
Aleksandr_Sakharov <mr.11aleksandrsasharov11@mail.ru>
2024-09-13 14:29:02 +0300

    434234

---
+++ Patient.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
+// #include <>
+
+
+struct Diag
+{
+    std::string desis;
+    std::string pils;
+};
+
+
+
+
+
+class Patient
+{
+private:
+    std::string _name;
+    std::string _patronomic;
+    std::string _surname;
+    Date _bornDate;
+    // UUID _patientID;
+    Diag _diag;
+    std::string _problem;
+    void ParseFIO(std::string FIO);
+
+public:
+    Patient(std::string name, std::string patronomic, std::string surname, std::string problem, Date bornDate);
+    Patient(std::string FIO, std::string problem, Date bornDate);
+    void PatientInfo();
+};
+
+
+struct Date
+{
+    std::byte day;
+    std::byte month;
+    uint16_t year;
+    uint32_t datecode;
+};
